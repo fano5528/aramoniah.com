cef78e20f2a5c66574fd429401a1561ac7968f0f
ops@fanodesign.com <josefano@MacBook-Air-96.local>
2023-04-21 00:12:36 -0600

    quitado signin

--- pages/sign-in.js
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs"
-
-export default function signin() {
-    return (
-        <div className="flex w-full h-[100vh] items-center justify-center">
-            <SignIn path="/sign-in" routing="path" classNam/>
-        </div>
-    )
-}
